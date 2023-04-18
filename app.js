6ea5595f3aa0d129773a5f27862c1ca729e716a2
Sanubar-Basirzada <7bqlyhs@code.edu.az>
2023-04-18 17:28:04 +0400

    fayl

---
+++ app.js
@@ -0,0 +1,104 @@
+// arr = ["masalli", "lenkeran", "baki", "lerik", "astara"];
+// sabit=arr.shift()
+// console.log(arr);
+///2
+// sabit=arr.reverse()
+// console.log(arr);
+////3
+// sabit=arr.map((x)=>x+"AZE")
+// console.log(sabit);
+//////3
+// let sabit;
+// function sonluq(x) {
+//   for (let i = 0; i < arr.length; i++) {
+//     sabit = arr.map((x, i) => {
+//       if (i % 2 == 1) {
+//         return x + "AZE";
+//       }
+//       else{
+//         return x
+//       }
+//     });
+//   }
+//   return sabit
+// }
+
+// console.log(sonluq());
+//////////4
+// arr.forEach(elemen=>console.log(elemen))
+// console.log(arr)
+/////5
+// const arr2 = ["Masalli", "lenkeran", "baki", "lerik", "astara"];
+// sabit=arr2.reduce((a,b) => {
+//       return a+b;
+// })
+// console.log(sabit)
+/////////6
+
+// const arr1 = arr2.length
+
+// console.log(arr1)
+////////////7
+// // for (let i = 0; i < arr2.length; i++) {
+// // if(i%2==0){
+// //     console.log(arr2[i]);
+// // }
+// // }
+///////////////8
+// const sabit = arr2.filter(arr2 => arr2.length > 6);
+// console.log(sabit);
+///////////////9
+
+// sabit=arr2.map((x)=>x.substring(0,3))
+// console.log(sabit);
+/////////////////10
+// sabit = arr2.filter((x) => x[0] !== "l");
+
+// console.log(sabit);
+///////////11
+// arr2[1] = ["mandalin", "limon"];
+
+// console.log(arr2);
+////////////////2.1
+const arr=[
+    {
+    "id": 2,
+    "description": "Sweet and savory sauces relishes spreads and seasonings",
+    "name": "Condiments"
+    },
+    {
+    "id": 1,
+    "description": "Soft drinks coffees teas beers and ales",
+    "name": "Beverages"
+    },
+    {
+    "id": 3,
+    "description": "Desserts candies and sweet breads",
+    "name": "Confections"
+    },
+    {
+    "id": 4,
+    "description": "Cheeses",
+    "name": "Dairy Products"
+    },
+    {
+    "id": 5,
+    "description": "Breads crackers pasta and cereal",
+    "name": "Grains/Cereals"
+    },
+    {
+    "id": 6,
+    "description": "Prepared meats",
+    "name": "Meat/Poultry"
+    },
+    {
+    "id": 7,
+    "description": "Dried fruit and bean curd",
+    "name": "Produce"
+    },
+    {
+    "id": 8,
+    "description": "Seaweed and fish",
+    "name": "Seafood"
+    }
+    ]
